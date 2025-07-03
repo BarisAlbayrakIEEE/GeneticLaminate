04d6ec35a2b18fd3077efb3dfa4b843500a1a1da
BarisAlbayrakIEEE <baris.albayrak.ieee@gmail.com>
2025-07-03 15:43:23 +0300

    c++ standard defaulted

--- build.sh
+++ build.sh
@@ -8,12 +8,11 @@ export PATH=${CUDA_PATH}/bin:${PATH}
 export LD_LIBRARY_PATH=${CUDA_PATH}/lib64:${LD_LIBRARY_PATH:-}
 
 SM_ARCH=sm_50
-STD_VER=c++17
+STD_VER=c++11 # not used, defaulted by nvcc
 SRC=genetic_stacking.cu
 OUT=genetic_stacking.out
 
 nvcc -ccbin gcc \
-  -std=${STD_VER} \
   -arch=${SM_ARCH} \
   -O3 \
   -rdc=true \
